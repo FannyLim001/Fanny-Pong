93ecdcb712f3170052922adc38a2d79642e5b7aa
Fanny <fanny20ti@mahasiswa.pcr.ac.id>
2023-12-08 10:34:18 +0700

    Adding speed up power up

---
+++ Assets/Scripts/PUSpeedUpController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PUSpeedUpController : MonoBehaviour
+{
+    public Collider2D ball;
+    public float magnitude;
+    public PowerUpManager manager;
+    private bool ballCollided = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision == ball)
+        {
+            // Speed Up the ball
+            ball.GetComponent<BallController>().ActivatePUSpeedUp(magnitude);
+            ballCollided = true;
+            RemovePowerUp();
+        }
+    }
+
+    private void Update()
+    {
+        if(!ballCollided)
+        {
+            Invoke("RemovePowerUp", manager.spawnInterval);
+        }
+    }
+
+    private void RemovePowerUp()
+    {
+        manager.RemovePowerUp(gameObject);
+    }
+}
